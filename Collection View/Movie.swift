85481e90d1a04fc4873db9a33f0a0da15d66962a
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2021-09-17 14:31:07 +0300

    first commit

---
+++ Collection View/Movie.swift
@@ -0,0 +1,8 @@
+//
+//  Movie.swift
+//  Collection View
+//
+//  Created by Abdirizak Hassan on 9/17/21.
+//
+
+import Foundation
---
+++ Collection View/MovieCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MovieCollectionViewCell.swift
+//  Collection View
+//
+//  Created by Abdirizak Hassan on 9/17/21.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+    
+}
