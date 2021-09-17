3ded21f98264e8e72cb091636e3abc6f3f8a7916
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2021-09-17 13:59:28 +0300

    Initial Commit

---
+++ Collection View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Collection View
+//
+//  Created by Abdirizak Hassan on 9/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
